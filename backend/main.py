cb4aa3f8f1559b93aa8a99847c46a26db36a02d1
ajaysingh822 <ajaysinghsisodiya8224@gmail.com>
2026-08-31 11:07:56 +0530

    Fix CORS for Netlify

--- backend/main.py
+++ backend/main.py
@@ -20,8 +20,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://illustrious-dieffenbachia-6ca75c.netlify.app"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
